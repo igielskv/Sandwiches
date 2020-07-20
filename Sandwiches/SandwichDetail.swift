2b1dfa5a7338c29d5ceb90c2c54a533bd94d006e
Vladislav Igielski <igielskv@icloud.com>
2020-07-20 11:04:19 +0200

    Adds animation to show / hide spicy banner when image is zoomed in / out.

--- Sandwiches/SandwichDetail.swift
+++ Sandwiches/SandwichDetail.swift
@@ -37,6 +37,7 @@ struct SandwichDetail: View {
                 .font(Font.headline.smallCaps())
                 .background(Color.red)
                 .foregroundColor(.yellow)
+                .transition(.move(edge: .bottom))
             }
         }
         .navigationBarTitle(sandwich.name)
